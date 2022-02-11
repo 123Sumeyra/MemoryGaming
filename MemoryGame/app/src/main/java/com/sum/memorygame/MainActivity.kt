97b0e1b7794857acd879aa7f1161762f5685ce4b
123Sumeyra <ozugursumeyra@gmail.com>
2022-02-11 13:13:44 +0300

    add Adapter

--- MemoryGame/app/src/main/java/com/sum/memorygame/MainActivity.kt
+++ MemoryGame/app/src/main/java/com/sum/memorygame/MainActivity.kt
@@ -2,10 +2,35 @@ package com.sum.memorygame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.GridLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.sum.memorygame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding :ActivityMainBinding
+    private lateinit var rvBoard: RecyclerView
+    private lateinit var tvNumMoves: TextView
+    private lateinit var tvNumPair: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        rvBoard = binding.rvboard
+        tvNumMoves =binding.tvNumMoves
+        tvNumPair = binding.tvNumPairs
+
+        rvBoard.adapter =MemoryBoardAdapter(this, 8)
+        rvBoard.setHasFixedSize(true)
+        rvBoard.layoutManager =GridLayoutManager(this, 2)
+
+
+
+
     }
 }
