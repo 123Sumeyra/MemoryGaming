a3a28417f91c1ed82c1d4c06a5240476c3933553
123Sumeyra <ozugursumeyra@gmail.com>
2022-02-11 19:51:58 +0300

    vector asset added

---
+++ MemoryGame/app/src/main/java/com/sum/memorygame/Constant.kt
@@ -0,0 +1,19 @@
+package com.sum.memorygame
+
+
+val  DEFAULT_ICONS = listOf<Int>(
+    R.drawable.ic_face,
+    R.drawable.ic_flight,
+    R.drawable.ic_flowar,
+    R.drawable.ic_gift,
+    R.drawable.ic_home,
+    R.drawable.ic_mood,
+    R.drawable.ic_person,
+    R.drawable.ic_school,
+    R.drawable.ic_send,
+    R.drawable.ic_smartphone,
+    R.drawable.ic_star,
+    R.drawable.ic_work,
+
+
+)
